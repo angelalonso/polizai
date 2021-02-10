c109edeae0b500847e7a0de7ac5d663557395ce3
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2021-02-10 22:25:28 +0100

    trying to get data from api

--- phrase_frontend/src/App.js
+++ phrase_frontend/src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import dotenv from "dotenv";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import Async from 'react-async';
 
+const loadUsers = () =>
+  fetch("http://172.17.0.2:8000")
+    .then(res => (res.ok ? res : Promise.reject(res)))
+    .then(res => res.text())
 
 function App() {
     var background = {backgroundSize : 'cover'};
@@ -27,6 +32,31 @@ function App() {
               src="http://www.milkbardigital.com.au/wp-content/uploads/2015/11/Milkbar-Home-Background.jpg"
             />
             <h1 style={textStyle}>{getPhrase}</h1>
+    <div className="container">
+      <Async promiseFn={loadUsers}>
+        {({ data, err, isLoading }) => {
+          if (isLoading) return "Loading..."
+          if (err) return `Something went wrong: ${err.message}`
+
+          if (data)
+            return (
+              <div>
+                <div>
+                  <h2>React Async - Random Users</h2>
+                </div>
+                {data.map(user=> (
+                  <div key={user} className="row">
+                    <div className="col-md-12">
+                      <p>{user.name}</p>
+                      <p>{user.email}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )
+        }}
+      </Async>
+    </div>
         </div>
     );
 }
