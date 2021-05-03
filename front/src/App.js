dc0c45088cfe389c011e6a42a7044837415c2926
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-05-03 18:39:17 +0200

    improving nesting and adding another layer

--- front/src/App.js
+++ front/src/App.js
@@ -22,25 +22,31 @@ class App extends React.Component {
       return count * 10;
     }
 
-  getObjects(nest_state_in) {
+  getObjects() {
+    //TODO: rename these variables 
     var data_sample = this.state.in_data;
     var nest_state = this.state.nest_state;
     var selected_arr = [];
+
     var selected_key = this.state.selected_key;
-    var parent_keys = selected_key.split("_");
+    var selected_key_nest_levels = selected_key.split("_");
     var selected_key_degrees = (selected_key.match(/_/g) || []).length;
     Object.keys(data_sample).forEach(function(key){
       var k = data_sample[key].k;
       var key_degrees = (k.match(/_/g) || []).length;
+      var key_nest_levels = k.split("_");
       // Show parents
-      for (let i =0; i < parent_keys.length; i++) {
-        if (k === parent_keys.slice(0, i).join("_")) {
+      for (let i =0; i < selected_key_nest_levels.length; i++) {
+        if (k === selected_key_nest_levels.slice(0, i).join("_")) {
           var full_object = data_sample[key];
           selected_arr.push(full_object);
         }
       }
       // Show "siblings"
-      if ((key_degrees === selected_key_degrees) && !(k === selected_key)) {
+      var key_parent = key_nest_levels.slice(0, -1).join('_');
+      var selected_key_parent = selected_key_nest_levels.slice(0, -1).join('_');
+      if ((key_degrees === selected_key_degrees) && !(k === selected_key) && 
+        (key_parent === selected_key_parent)) {
           var full_object = data_sample[key];
           selected_arr.push(full_object);
       }
@@ -61,6 +67,8 @@ class App extends React.Component {
   }
 
   getKey(selected_key) {
+    var selected_key_degrees = (selected_key.match(/_/g) || []).length;
+    var state_selected_key_degrees = (this.state.selected_key.match(/_/g) || []).length;
     if (selected_key === this.state.selected_key) {
       if (this.state.nest_state === "open") {
         var new_nest_state = "closed";
@@ -68,14 +76,18 @@ class App extends React.Component {
         var new_nest_state = "open";
       }
     } else {
-      new_nest_state = "open";
+      if (selected_key_degrees < state_selected_key_degrees) {
+        new_nest_state = "closed";
+      } else {
+        new_nest_state = "open";
+      }
     }
 
     this.setState({
       selected_key: selected_key,
       nest_state: new_nest_state,
     }, function () {
-        var key_objects = this.getObjects(new_nest_state);
+        var key_objects = this.getObjects();
         this.setState({
           shown_data: key_objects,
         })
@@ -98,8 +110,8 @@ class App extends React.Component {
     </ul>;
   }
 
-  UNSAFE_componentWillMount() {
-    var key_objects = this.getObjects("open");
+  componentDidMount() {
+    var key_objects = this.getObjects();
     this.setState({
       shown_data: key_objects,
 
--- front/src/data.js
+++ front/src/data.js
@@ -42,6 +42,20 @@ export const inData = [
     "childof": "00_01"
   },
   { 
+    "k": "00_01_04_01",
+    "name": "Coal",
+    "amount": 60,
+    "percent": 60,
+    "childof": "00_01"
+  },
+  { 
+    "k": "00_01_04_02",
+    "name": "Others",
+    "amount": 60,
+    "percent": 60,
+    "childof": "00_01"
+  },
+  { 
     "k": "00_01_05",
     "name": "Transport",
     "amount": 60,
@@ -58,36 +72,36 @@ export const inData = [
   { 
     "k": "00_02_01",
     "name": "Buildings",
-    "amount": 60,
-    "percent": 60,
+    "amount": 40,
+    "percent": 40,
     "childof": "00_02"
   },
   { 
     "k": "00_02_02",
     "name": "Non Combustion",
-    "amount": 60,
-    "percent": 60,
+    "amount": 40,
+    "percent": 40,
     "childof": "00_02"
   },
   { 
     "k": "00_02_03",
     "name": "Other Industrial Combustion",
-    "amount": 60,
-    "percent": 60,
+    "amount": 40,
+    "percent": 40,
     "childof": "00_02"
   },
   { 
     "k": "00_02_04",
     "name": "Power Industry",
-    "amount": 60,
-    "percent": 60,
+    "amount": 40,
+    "percent": 40,
     "childof": "00_02"
   },
   { 
     "k": "00_02_05",
     "name": "Transport",
-    "amount": 60,
-    "percent": 60,
+    "amount": 40,
+    "percent": 40,
     "childof": "00_02"
   }
 ];
