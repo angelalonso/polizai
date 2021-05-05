e7d1b575668ccb11c10c8a6b39e3c8265f501327
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-05-05 14:35:31 +0200

    data prep working

--- data_worker/src/main.rs
+++ data_worker/src/main.rs
@@ -1,33 +1,146 @@
+#![feature(with_options)]
 extern crate csv;
+extern crate serde_json;
+#[macro_use] extern crate serde;
 
-use std::env;
-use std::error::Error;
-use std::ffi::OsString;
+use std::fs;
 use std::fs::File;
 use std::process;
 
-fn run() -> Result<(), Box<Error>> {
-    let file_path = get_first_arg()?;
-    let file = File::open(file_path)?;
-    let mut rdr = csv::Reader::from_reader(file);
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Entry {
+    k: String,
+    name: String,
+    amount: f32,
+    percent: f32,
+    childof: String,
+}
+
+fn load_edgar() -> Result<()> {
+    let mut all_entries: Vec<Entry> = [].to_vec();
+    let file_path = "../EDGAR_fossil_CO2_totals_by_country.csv";
+    let head_f = match File::open(file_path) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("{:?}", e);
+            std::process::exit(2);
+        }
+    };
+    let mut head_rdr = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(head_f);
+    // get "Global Total" entry first
+    let mut totalco2 = 0.0;
+    let mut ix_totalco2 = 0.0;
+    for result in head_rdr.records() {
+        let record = match result {
+            Ok(r) => Some(r),
+            Err(_err) => {
+                None
+            }
+        };
+        // we clean up manually here because we dont need the other 48 fields now
+        match record {
+            Some(r) => {
+                let country = r.get(0);
+                let co2_2019 = r.get(50);
+                let co2_amount = match co2_2019.unwrap().parse::<f32>() {
+                    Ok(i) => i,
+                    Err(_err) => 0.0
+                };
+                if country == Some("GLOBAL TOTAL") {
+                    let e = Entry {
+                        k: "00".to_string(),
+                        name: country.unwrap().to_string(),
+                        amount: co2_amount,
+                        percent: 100.0,
+                        childof: "".to_string(),
+                    };
+                    all_entries.push(e);
+                    totalco2 = co2_amount;
+                };
+
+                ()
+            },
+            None => ()
+        }
+    };
+    //  get the rest
+    let mut key = 0;
+    let f = match File::open(file_path) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("{:?}", e);
+            std::process::exit(2);
+        }
+    };
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(f);
     for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
+        let record = match result {
+            Ok(r) => Some(r),
+            Err(_err) => {
+                None
+            }
+        };
+        // we clean up manually here because we dont need the other 48 fields now
+        match record {
+            Some(r) => {
+                let country = r.get(0);
+                let co2_2019 = r.get(50);
+                let co2_amount = match co2_2019.unwrap().parse::<f32>() {
+                    Ok(i) => i,
+                    Err(_err) => 0.0
+                };
+                if (country != Some("GLOBAL TOTAL")) && 
+                    (country != Some("EU27+UK")) &&
+                    (country != Some("")) {
+                    key += 1;
+                    let formatted_key = format!("00_{}", key);
+                    let this_percent = co2_amount * 100.0 / totalco2;
+                    let e = Entry {
+                        k: formatted_key,
+                        name: country.unwrap().to_string(),
+                        amount: co2_amount,
+                        percent: this_percent,
+                        childof: "00".to_string(),
+                    };
+                    ix_totalco2 += co2_amount;
+                    all_entries.push(e);
+                };
+
+                ()
+            },
+            None => ()
+        }
     }
+
+    println!("{:#x?}", all_entries);
+    // check the sum of all is the same as the global total
+    //println!("{:#x?}", ix_totalco2);
+    //println!("{:#x?}", totalco2);
+    ::serde_json::to_writer(&File::create("data_json_temp").unwrap(), &all_entries)?;
+    let json_start = "export const inData = ";
+    fs::write("data.js", json_start).expect("Unable to write to file");
+    ::serde_json::to_writer(&File::with_options().append(true).open("data.js").unwrap(), &all_entries)?;
     Ok(())
 }
 
-/// Returns the first positional argument sent to this process. If there are no
-/// positional arguments, then this returns an error.
-fn get_first_arg() -> Result<OsString, Box<Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
-    }
-}
+// /// Returns the first positional argument sent to this process. If there are no
+// /// positional arguments, then this returns an error.
+// fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+//     match env::args_os().nth(1) {
+//         None => Err(From::from("expected 1 argument, but got none")),
+//         Some(file_path) => Ok(file_path),
+//     }
+// }
 
 fn main() {
-    if let Err(err) = run() {
+    if let Err(err) = load_edgar() {
         println!("{}", err);
         process::exit(1);
     }
--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import './App.css';
-import { inData } from "./data";
+import '../style/App.css';
+import { inData } from "../data/data";
 
 class App extends React.Component {
   constructor(props) {
@@ -24,46 +24,46 @@ class App extends React.Component {
 
   getObjects() {
     //TODO: rename these variables 
-    var data_sample = this.state.in_data;
+    var data_original = this.state.in_data;
     var nest_state = this.state.nest_state;
-    var selected_arr = [];
+    var result = [];
 
     var selected_key = this.state.selected_key;
-    var selected_key_nest_levels = selected_key.split("_");
-    var selected_key_degrees = (selected_key.match(/_/g) || []).length;
-    Object.keys(data_sample).forEach(function(key){
-      var k = data_sample[key].k;
-      var key_degrees = (k.match(/_/g) || []).length;
-      var key_nest_levels = k.split("_");
+    var selected_key_layers = selected_key.split("_");
+    var selected_key_layer_nr = (selected_key.match(/_/g) || []).length;
+    Object.keys(data_original).forEach(function(key){
+      var k = data_original[key].k;
+      var key_layers = k.split("_");
+      var key_layer_nr = (k.match(/_/g) || []).length;
       // Show parents
-      for (let i =0; i < selected_key_nest_levels.length; i++) {
-        if (k === selected_key_nest_levels.slice(0, i).join("_")) {
-          var full_object = data_sample[key];
-          selected_arr.push(full_object);
+      for (let i =0; i < selected_key_layers.length; i++) {
+        if (k === selected_key_layers.slice(0, i).join("_")) {
+          var full_object = data_original[key];
+          result.push(full_object);
         }
       }
       // Show "siblings"
-      var key_parent = key_nest_levels.slice(0, -1).join('_');
-      var selected_key_parent = selected_key_nest_levels.slice(0, -1).join('_');
-      if ((key_degrees === selected_key_degrees) && !(k === selected_key) && 
+      var key_parent = key_layers.slice(0, -1).join('_');
+      var selected_key_parent = selected_key_layers.slice(0, -1).join('_');
+      if ((key_layer_nr === selected_key_layer_nr) && !(k === selected_key) && 
         (key_parent === selected_key_parent)) {
-          var full_object = data_sample[key];
-          selected_arr.push(full_object);
+          var full_object = data_original[key];
+          result.push(full_object);
       }
       // SHow itself and children
       if (nest_state === "closed") {
         if (k === selected_key) {
-          var full_object = data_sample[key];
-          selected_arr.push(full_object);
+          var full_object = data_original[key];
+          result.push(full_object);
         }
       } else {
-        if ((k === selected_key) || ((k.includes(selected_key + "_")) && !( key_degrees > selected_key_degrees + 1))) {
-          var full_object = data_sample[key];
-          selected_arr.push(full_object);
+        if ((k === selected_key) || ((k.includes(selected_key + "_")) && !( key_layer_nr > selected_key_layer_nr + 1))) {
+          var full_object = data_original[key];
+          result.push(full_object);
         }
       }
     })
-    return selected_arr
+    return result
   }
 
   getKey(selected_key) {
