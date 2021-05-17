36018854d77ef01585f55cc9f845941b94a3f607
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-05-17 23:04:05 +0200

    modifying the dump completely

--- database/k8s/config.sh
+++ database/k8s/config.sh
@@ -17,5 +17,10 @@ sed -i -e "s|\$DB_USER|$DB_USER|g" secret.yaml
 sed -i -e "s|\$DB_PASS|$DB_PASS|g" secret.yaml
 
 
+# Correct the dump: MODIFY THIS IF YOU CREATED THE DUMP WITH OTHER USER/SCHEMA NAMES!!
 sed -i -e "s|Owner: postgres|Owner: $user|g" ../data/full_datadump.sql
 sed -i -e "s|OWNER TO postgres|OWNER TO $user|g" ../data/full_datadump.sql
+
+sed -i -e "s|Schema: public|Schema $name|g" ../data/full_datadump.sql
+sed -i -e "s|COPY public\.co2|COPY $name.co2|g" ../data/full_datadump.sql
+sed -i -e "s|TABLE public\.co2|TABLE $name.co2|g" ../data/full_datadump.sql
