90e467bf9a375ed99825885871343830eda1c98f
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-05-29 09:25:35 +0200

    got something that works a bit

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -4,6 +4,20 @@ import '../style/App.css';
 import axios from 'axios';
 import List from './List';
 import withListLoading from './withListLoading';
+// TODO:
+// chain like this:
+//   axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p1)
+//  .then(response => {
+//    this.setState({ p1Location: response.data });
+//    return axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p2);
+//  })
+//  .then(response => {
+//    this.setState({ p2Location: response.data });
+//    return axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p3);
+//  })
+//  .then(response => {
+//    this.setState({ p3Location: response.data });
+//  }).catch(error => console.log(error.response));
 
 function App() {
   const ListLoading = withListLoading(List);
@@ -15,7 +29,7 @@ function App() {
   useEffect(() => {
     setAppState({ loading: true });
     const apiUrl = `http://0.0.0.0:8000/api/co2-countries/get_main`;
-    const apiJWTToken = "xxx"
+    const apiJWTToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjIyNzA1NDcsImV4cCI6MTYyMjg3NTM0NywidXNlciI6InRlc3QiLCJsb2dpbl9zZXNzaW9uIjoiYzgwOTEyNjEyMDJlNGE0OGI0YjMyZDI2NzNmNTdkNTMifQ.KJP0qp7wFBHL6h10fok9FVIWjKAX2FHlBdbcR7puC-A"
     const apiConfig = {
        headers: {
           Authorization: "Bearer " + apiJWTToken
--- front/src/components/List.js
+++ front/src/components/List.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
+function doIndentation(this_key) {
+  var count = (this_key.match(/_/g) || []).length;
+  console.log(count);
+  return count * 10;
+}
+
 const List = (props) => {
   const { repos } = props;
   if (!repos || repos.length === 0) return <p>No repos, sorry</p>;
 
 
-  console.log(repos);
   return (
     <ul>
       {repos.data.map((item) => {
         return (
-          <div key={item.key} style={{display: "block"}} className="button_main" >
+          <div key={item.key} style={{display: "block"}} className="button_main" style={{ marginLeft: doIndentation("00") }}>
             <Button variant="contained" style={{display: "grid", width: "99%"}} >
               <div className="button_name" style={{position: "absolute", alignSelf: "center", left: 0}} >{item.country_name}</div>
               <div className="button_amount" style={{alignSelf: "center"}}>{Math.round(item.amount_2019 * 10) / 10} Ton./Yr.</div>
