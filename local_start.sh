d9fabae11df80c0fde7c4ad60ccb0584b09e1d05
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-06-17 22:08:01 +0200

    preparing for JWT TOKEN and local starts

--- local_start.sh
+++ local_start.sh
@@ -18,14 +18,27 @@ back() {
   cd $CWD/back; cargo run
 }
 
+front() {
+  # Start Backend
+  export REACT_APP_API_URL="http://127.0.0.1:8000"
+  export REACT_APP_JWT_TOKEN="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjM5NTc2MDksImV4cCI6MTYyNDU2MjQwOSwidXNlciI6InRlc3QiLCJsb2dpbl9zZXNzaW9uIjoiNjMyMjRmYzg5MmZmNDUyMWI4YmZiNjBjYmRmZTdhY2EifQ.yOP-WzkUsb2zsULeZE2Doe6nTcpLV8-lJIxEGf7aZBI"
+  cd $CWD/front; npm start
+}
+
 test_api() {
   # Authenticate to Backend and get token
   BACKIP="127.0.0.1"
-  CMD_2="curl --header \"Content-Type: application/json\" --request POST --data '{\"username\": \"test\", \"email\": \"test@test.com\", \"password\": \"pass1234\"}' http://$BACKIP:8000/api/auth/signup"
-  terminator -T "<NAME-1>" -e "${CMD_2}" &
+  curl --header "Content-Type: application/json" --request POST --data '{"username": "test", "email": "test@test.com", "password": "pass1234"}' http://$BACKIP:8000/api/auth/signup
   TOKEN=$(curl --header "Content-Type: application/json" --request POST --data '{"username_or_email": "test", "password": "pass1234"}' http://$BACKIP:8000/api/auth/login | jq '.data.token')
   echo Token: $TOKEN
 }
 
-back
-
+if [[ "$1" == "db" || "$1" == "data" || "$1" == "database" ]]; then
+  db
+elif [[ "$1" == "back" || "$1" == "backend" || "$1" == "api" ]]; then
+  back
+elif [[ "$1" == "test" ]]; then
+  test_api
+elif [[ "$1" == "front" || "$1" == "frontend" ]]; then
+  front
+fi
